74099a37ef28f290c41d6598f4950d543683ff49
KrushnaPhad <krushna.phad@doodlodesigns.com>
2024-03-21 15:15:39 +0530

    1.R&D TAX CREDITS page-Clickrnd removed 2. Useful Links - check eligibility - Clickrnd removed 3. Privacy Policy - Changed company reg number and address 4. T&C  - Replaced all More group name with Enso 5. Complaint Procesdure  - Replaced all More group name with Enso

--- application/views/privacy_policy.php
+++ application/views/privacy_policy.php
@@ -96,14 +96,14 @@
                  <div class="container">
                  <h2 class="mt-4">Contact details</h2>
                  <p class="mt-4">Our full details are:</p>
-                 <p class="mt-4">Full name of legal entity: Phoenix R&D  Limited (company number 10580082)</p>
+                 <p class="mt-4">Full name of legal entity: Phoenix R&D  Limited (company number 15270538)</p>
                  <p class="mt-4">Person responsible for data protection: the data privacy manager</p>
                  <p class="mt-4">Email address: you can contact the data privacy manager here.</p>
-                 <p class="mt-4">Postal address: Phoenix R&D 71-75 Shelton Street, Covent Garden, London, WC2H 9JQ.</p>
+                 <p class="mt-4">Postal address: Phoenix R&D Flat 86 Ember Court, Emlyn Gardens, London, England, W12 9UE.</p>
                  <p class="mt-4">Telephone number:</p>
                  <p class="mt-4">New Clients Department – 0208 1257590</p>
                  <p class="mt-4">Existing Clients Queries Department – 0208 1257591</p>
-                 <p class="mt-4">More Group: 0203 481 6709</p>
+                 <p class="mt-4">Enso: 0203 481 6709</p>
                  <p class="mt-4">You have the right to make a complaint at any time to the Information Commissioner’s Office (ICO), the UK supervisory authority for data protection issues (www.ico.org.uk). We would, however, appreciate the chance to deal with your concerns before you approach the ICO so please contact us in the first instance.</p>
                  <p class="mt-4">Changes to the Privacy Notice and your duty to inform us of changes</p> 
                  <p class="mt-4">This version was last updated on 10 June 2020 and historic versions can be obtained by contacting us.</p>
