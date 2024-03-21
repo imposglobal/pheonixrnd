756f908be783e8c575220b72c6264cddcea78f13
KrushnaPhad <krushna.phad@doodlodesigns.com>
2024-03-21 10:46:15 +0530

    base url update

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://clickrnd.imposglobal.com/';
-$config['base_url'] = 'http://localhost/pheonixrnd/pheonixrnd/';
+$config['base_url'] = 'https://clickrnd.imposglobal.com/';
+// $config['base_url'] = 'http://localhost/pheonixrnd/pheonixrnd/';
 
 
 
